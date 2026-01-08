ed89986b0f34d1ac41cf09d4995072d564763771
Theodore <theodorez888@gmail.com>
2026-01-08 12:14:33 -0800

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import React from "react"
+import Head from "next/head"
 
 import "../styles/globals.css"
 
 // This default export is required in a new `pages/_app.tsx` file.
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
